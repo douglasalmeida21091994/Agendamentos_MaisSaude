27979cefe3789722ab842692e267d5f3475f8ae5
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2024-12-04 02:34:15 -0300

    ajustando img

--- index.php
+++ index.php
@@ -33,11 +33,14 @@ $dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agendamentos Mais Saúde</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
 </head>
 
 <body>
     <div class="header">
         <header class="title">
+        <img src="img/nova-logo-branca 1.png" alt="Logo Smile Saúde" class="title-image">
             <h2>Agendamentos Mais Saúde</h2>
         </header>
     </div>
