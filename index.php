6d7e196c14a085113feec4a79f890cb7958af285
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2024-12-03 22:15:16 -0300

    Agendamentos Mais Saúde

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agendamentos Mais Saúde</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css" integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="header">
+
+        <header class="title">
+            <img src="img/nova-logo-branca 1.png" alt="Logo Smie Saúde" class="title-image">
+            <h2>Agendamentos Mais Saúde</h2>
+        </header>
+
+    </div>
+
+    <div class="main">
+        <form action="processa.php" method="post"></form>
+    </div>
+</body>
+
+</html>
